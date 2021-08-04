283a2b62651e7bf7839b57f93b0d9f2c6662edcc
Steven Liu <xian.f.liu@gmail.com>
2021-08-04 22:58:28 +0100

    feat: voting keys generator (#31)
    * feat: voting keys generator
    
    * fix: PR #31 comments
    
    - Typo fixed in unit test
    - Remvoed redundant param in .subarray(..) in unit test
    - Change from Uint8Array.set to Buffer.fill in VotingKeyGenerator

---
+++ src/core/symbol/VotingKeysGenerator.ts
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2021 SYMBOL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { KeyPair } from '../KeyPair';
+import { Converter } from '../utils';
+import { SymbolKeyPair } from './SymbolKeyPair';
+
+export class VotingKeysGenerator {
+    /**
+     * Constructor
+     * @param {KePair} rootKeyPair The root keypair
+     * @param keyPairGenerator Symbol key pair generator
+     */
+    constructor(private readonly rootKeyPair: KeyPair, private readonly keyPairGenerator = SymbolKeyPair.generate) {}
+
+    /**
+     * Generate Symbol voting keys
+     * @param {number} startEpoch Start voting key epoch
+     * @param {number} endEpoch End voting key epoch
+     * @returns {Uint8Array}
+     */
+    public generate(startEpoch: number, endEpoch: number): Uint8Array {
+        const items = endEpoch - startEpoch + 1;
+        const headerSize = 64 + 16;
+        const itemSize = 32 + 64;
+        const totalSize = headerSize + items * itemSize;
+        const buffer = Buffer.alloc(totalSize)
+            .fill(Converter.numberToUint8(startEpoch, 8), 0)
+            .fill(Converter.numberToUint8(endEpoch, 8), 8)
+            .fill(Converter.hexToUint8('FFFFFFFFFFFFFFFF'), 16)
+            .fill(Converter.hexToUint8('FFFFFFFFFFFFFFFF'), 24)
+            .fill(this.rootKeyPair.publicKey.toBytes(), 32)
+            .fill(Converter.numberToUint8(startEpoch, 8), 64)
+            .fill(Converter.numberToUint8(endEpoch, 8), 72);
+
+        for (let i = 0; i < items; i++) {
+            const randomKeyPair = this.keyPairGenerator();
+            buffer.fill(randomKeyPair.privateKey.toBytes(), 80 + i * itemSize);
+            const identifier = Converter.numberToUint8(endEpoch - i, 8);
+            const signature = this.rootKeyPair.sign(Uint8Array.from([...randomKeyPair.publicKey.toBytes(), ...identifier]));
+            buffer.fill(signature, 80 + 32 + i * itemSize);
+        }
+        return buffer;
+    }
+}
---
+++ test/core/symbol/VotingKeysGenerator.spec.ts
@@ -0,0 +1,71 @@
+/*
+import { SymbolKeyPair } from '../../../src/core/symbol/SymbolKeyPair';
+ * Copyright 2021 SYMBOL
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { toBufferLE } from 'bigint-buffer';
+import { expect } from 'chai';
+import { Key } from '../../../src/core/Key';
+import { SymbolKeyPair } from '../../../src/core/symbol/SymbolKeyPair';
+import { VotingKeysGenerator } from '../../../src/core/symbol/VotingKeysGenerator';
+import { Converter } from '../../../src/core/utils/Converter';
+
+describe('VotingKeysGenerator', () => {
+    it('can generate voting keys header', () => {
+        // Arrange:
+        const rootKeyPair = SymbolKeyPair.generate();
+        const generator = new VotingKeysGenerator(rootKeyPair);
+
+        // Act:
+        const votingKeys = generator.generate(7, 11);
+
+        // Assert:
+        expect(votingKeys.length).equal(32 + 32 + 16 + 5 * (32 + 64));
+
+        expect(Converter.uint8ToNumber(votingKeys.subarray(0, 8))).equal(7);
+        expect(Converter.uint8ToNumber(votingKeys.subarray(8, 16))).equal(11);
+        expect(Converter.uint8ToHex(votingKeys.subarray(16, 24))).equal('FFFFFFFFFFFFFFFF');
+        expect(Converter.uint8ToHex(votingKeys.subarray(24, 32))).equal('FFFFFFFFFFFFFFFF');
+
+        expect(votingKeys.subarray(32, 64)).deep.equal(rootKeyPair.publicKey.toBytes());
+        expect(Converter.uint8ToNumber(votingKeys.subarray(64, 72))).equal(7);
+        expect(Converter.uint8ToNumber(votingKeys.subarray(72, 80))).equal(11);
+    });
+
+    it('can generate random child keys', () => {
+        // Arrange:
+        const rootKeyPair = SymbolKeyPair.generate();
+        const generator = new VotingKeysGenerator(rootKeyPair);
+
+        // Act:
+        const votingKeys = generator.generate(7, 11);
+        let bytesWithoutHeader = votingKeys.subarray(32 + 32 + 16);
+
+        // Assert:
+        expect(votingKeys.length).equal(32 + 32 + 16 + 5 * (32 + 64));
+
+        for (let i = 4; i >= 0; i--) {
+            const childKeyPair = new SymbolKeyPair(new Key(bytesWithoutHeader.subarray(0, 32)));
+            const signature = bytesWithoutHeader.subarray(32, 32 + 64);
+
+            const payload = new Uint8Array(32 + 8);
+            payload.set(childKeyPair.publicKey.toBytes(), 0);
+            payload.set(toBufferLE(BigInt(7 + i), 8), 32);
+
+            expect(rootKeyPair.verify(payload, signature)).to.be.true;
+            bytesWithoutHeader = bytesWithoutHeader.subarray(32 + 64);
+        }
+    });
+});
