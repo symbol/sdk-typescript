9720007b092444645ede339f2d64ee133a704d43
Steven Liu <xian.f.liu@gmail.com>
2021-08-06 14:23:36 +0100

    fix: fixed in eslintrs.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,8 @@ module.exports = {
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        'plugin:@typescript-eslint/recommended',
     ],
     parserOptions: {
-        project: './tsconfig.json',
-        tsconfigRootDir: __dirname,
         ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
         sourceType: 'module', // Allows for the use of imports
     },
